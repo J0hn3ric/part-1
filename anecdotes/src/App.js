8e19116e0213912e01f6dec5398e5e4018973e29
J0hn3ric <john.eric.arriola@gmail.com>
2022-05-11 11:16:39 +0200

    fff

---
+++ anecdotes/src/App.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+const Button = (props) => {
+  return (
+      <button onClick = {props.onClick}>{props.name}</button>
+  )
+}
+
+const Anecdotes = (props) => {
+  return(
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.anecdotes} <br></br>
+      has {props.votes} votes</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const handleRandom = () => {
+    const random = Math.floor(Math.random() * 7)
+    setSelected(random)
+  }
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] +=1
+    setVotes(copy)
+    let max = 0
+    let cont = 0
+    for(let i = 0; i<copy.length; i++)
+      if(max<copy[i]){
+        max = copy[i]
+        cont = i
+      }
+    setTop(cont)
+  }
+
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const [top, setTop] = useState(0)
+
+  const [votes, setVotes] = useState(Array(7).fill(0))
+
+  return (
+    <div >
+      <Anecdotes title = {'Anecdote of the day'}
+        anecdotes = {anecdotes[selected]}
+        votes = {votes[selected]} />
+      <div>
+        <Button onClick = {handleVote} name = {'vote'} /> 
+        <Button onClick = {handleRandom} name = {'next Anecdote'} />
+      </div>
+      <Anecdotes title = {'Anecdote with most votes'}
+        anecdotes = {anecdotes[top]}
+        votes = {votes[top]} />
+    </div>
+  );
+}
+
+export default App;
